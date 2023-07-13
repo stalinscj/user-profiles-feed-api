ade30592851e52be02bf1cb41f94997adbefec05
stalinscj <ssanchez684@gmail.com>
2023-07-13 00:24:19 -0400

    10-004: Defining the UserProfileViewSet

--- profiles_api/views.py
+++ profiles_api/views.py
@@ -4,6 +4,7 @@ from rest_framework import viewsets
 from rest_framework.views import APIView
 from rest_framework.response import Response
 
+from profiles_api import models
 from profiles_api import serializers
 
 # Create your views here.
@@ -94,3 +95,8 @@ class HelloViewSet(viewsets.ViewSet):
     def destroy(self, request, pk=None):
         """Handle removing an object"""
         return Response({'http_method': 'DELETE'})
+
+class UserProfileViewSet(viewsets.ModelViewSet):
+    """Handle creating and updating profiles"""
+    serializer_class = serializers.UserProfileSerializer
+    queryset         = models.UserProfile.objects.all()

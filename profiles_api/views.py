6f1037229884a96381acdcfd9a72a6ccc21b4405
stalinscj <ssanchez684@gmail.com>
2023-07-13 00:52:42 -0400

    10-010: Adding the filter and search fields to UserProfileViewSet

--- profiles_api/views.py
+++ profiles_api/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from rest_framework import filters
 from rest_framework import status
 from rest_framework import viewsets
 from rest_framework.views import APIView
@@ -104,3 +105,5 @@ class UserProfileViewSet(viewsets.ModelViewSet):
     queryset               = models.UserProfile.objects.all()
     authentication_classes = (TokenAuthentication,)
     permission_classes     = (permissions.UpdateOwnProfile,)
+    filter_backends        = (filters.SearchFilter,)
+    search_fields          = ('name', 'email',)
